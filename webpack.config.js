52831dfb91566e6256bb48bb86c980ddf99e9c95
gregonrails <g314richards@gmail.com>
2018-11-22 20:48:38 +0900

    completed desktop and mobile header styles

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+//webpack wants an absolute path not a relative one
+var path = require('path');
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output:{
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename:"App.js"
+  },
+  module:{
+    rules:[
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ["@babel/preset-env"]
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
